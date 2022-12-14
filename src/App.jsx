08c17ba765211e7efeb3ed3bc51bd831a9d33a37
Vectorall <hectorverduzcorayo@gmail.com>
2022-12-14 13:34:39 -0800

    Ruby Ring

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import React, { useRef, useState } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
 import './App.css'
+import { OrbitControls, Stars } from '@react-three/drei';
 
-function App() {
-  const [count, setCount] = useState(0)
+
+function MyDonut({ rotSpeed, freqX = 1, ampX = 1, freqY = 1, ampY = 1 }) {
+  const meshRef = useRef(this);
+
+  useFrame(({clock}) => {
+    if (!meshRef.current) return;
+   
+    const t = clock.getElapsedTime();
+    meshRef.current.rotation.y = t * rotSpeed;
+    meshRef.current.position.x = -Math.cos(t * freqX) * ampX;
+    meshRef.current.position.y = Math.sin(t * freqY) * ampY;
+  })
 
   return (
+    <mesh ref={meshRef}>
+      <torusGeometry args={[1, 0.3, 20, 20]} />
+      <meshStandardMaterial color={"gold"} />
+      <mesh>
+        <dodecahedronGeometry />
+        <meshStandardMaterial color={"red"} />
+      </mesh>
+    </mesh>
+  )
+}
+
+function App() {
+  return (
     <div className="App">
-      <h1>Interesting</h1>
+      <Canvas>
+        <color attach="background" args={[0.01, 0.01, 0.01]} />
+        <ambientLight intensity={1} />
+        <directionalLight insensity={1} />
+        <Stars />
+        <MyDonut 
+          rotSpeed={Math.PI * 2} 
+          freqX={3}
+          ampX={3} 
+          freqY={3}
+          ampY={1}
+        />
+        <OrbitControls />
+      </Canvas>
     </div>
   )
 }
