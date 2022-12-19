272bf03e5b60279e814d741711220f63c85cbd23
Vectorall <hectorverduzcorayo@gmail.com>
2022-12-19 15:03:42 -0800

    basic sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,31 @@
-import React, { useRef, useState } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
+import { Canvas } from '@react-three/fiber'
+import { Html, Stars } from '@react-three/drei';
 import './App.css'
-import { OrbitControls, Stars } from '@react-three/drei';
 
-
-function MyDonut({ rotSpeed, freqX = 1, ampX = 1, freqY = 1, ampY = 1 }) {
-  const meshRef = useRef(this);
-
-  useFrame(({clock}) => {
-    if (!meshRef.current) return;
-   
-    const t = clock.getElapsedTime();
-    meshRef.current.rotation.y = t * rotSpeed;
-    meshRef.current.position.x = -Math.cos(t * freqX) * ampX;
-    meshRef.current.position.y = Math.sin(t * freqY) * ampY;
-  })
-
-  return (
-    <mesh ref={meshRef}>
-      <torusGeometry args={[1, 0.3, 20, 20]} />
-      <meshStandardMaterial color={"gold"} />
-      <mesh>
-        <dodecahedronGeometry />
-        <meshStandardMaterial color={"red"} />
-      </mesh>
-    </mesh>
-  )
-}
 
 function App() {
   return (
     <div className="App">
+      <header>
+
+      </header>
       <Canvas>
         <color attach="background" args={[0.01, 0.01, 0.01]} />
         <ambientLight intensity={1} />
         <directionalLight insensity={1} />
         <Stars />
-        <MyDonut 
-          rotSpeed={Math.PI * 2} 
-          freqX={3}
-          ampX={3} 
-          freqY={3}
-          ampY={1}
-        />
-        <OrbitControls />
       </Canvas>
+      <div className='sidebar'>
+        <ul>
+          <li><a href="">HOME</a></li>
+          <li><a href="">PROJECTS</a></li>
+          <li><a href="">EXPERIENCE</a></li>
+          <li><a href="">CONTACT</a></li>
+        </ul>
+      </div>
+      <footer>
+
+      </footer>
     </div>
   )
 }
